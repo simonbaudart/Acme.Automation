1527543b386c90d1313858fac64c961fa182ec3f
Simon Baudart <s.baudart@wavenet.be>
2019-10-21 17:07:43 +0200

    Add rule to check if it's a Curve Receipt

---
+++ Acme.Automation/Acme.Automation.Rules/CurveReceipt.cs
@@ -0,0 +1,27 @@
+//  <copyright file="CurveReceipt.cs" company="Acme">
+//  Copyright (c) Acme. All rights reserved.
+//  </copyright>
+
+namespace Acme.Automation.Rules
+{
+    using System;
+    using System.Linq;
+
+    using Acme.Automation.Core;
+
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Rule to check if message is from curve.
+    /// </summary>
+    public class CurveReceipt : IRule
+    {
+        /// <inheritdoc />
+        public bool IsMatch(JToken config, Message message)
+        {
+            var title = message.Items["subject"].ToString();
+
+            return title.Contains("Curve Receipt:");
+        }
+    }
+}
