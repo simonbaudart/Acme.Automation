990673ef968a88d8788665e78316b189d4aa4a7d
Simon Baudart <s.baudart@wavenet.be>
2019-11-19 15:36:00 +0100

    Start jobs from configuration

---
+++ Acme.Automation/Acme.Automation.WebRunner/AutomationRunner.cs
@@ -0,0 +1,17 @@
+// <copyright file="AutomationRunner.cs" company="Acme">
+// Copyright (c) Acme. All rights reserved.
+// </copyright>
+
+namespace Acme.Automation.WebRunner
+{
+    using Acme.Automation.Core;
+    using Acme.Automation.Core.Configuration;
+
+    public class AutomationRunner
+    {
+        public void Execute(AutomationConfiguration configuration, Job job)
+        {
+            new Worker().Execute(configuration, job);
+        }
+    }
+}
--- Acme.Automation/Acme.Automation.WebRunner/Startup.cs
+++ Acme.Automation/Acme.Automation.WebRunner/Startup.cs
@@ -5,10 +5,15 @@
 namespace Acme.Automation.WebRunner
 {
     using System;
+    using System.IO;
     using System.Linq;
+
+    using Acme.Automation.Core.Configuration;
 
     using Hangfire;
     using Hangfire.SqlServer;
+
+    using log4net;
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
@@ -18,12 +23,20 @@ namespace Acme.Automation.WebRunner
 
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        /// <summary>
+        /// Define the logger.
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetLogger(typeof(Startup));
+
+        public Startup(IConfiguration configuration, IHostingEnvironment environment)
         {
             this.Configuration = configuration;
+            this.Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+
+        public IHostingEnvironment Environment { get; }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -34,6 +47,7 @@ namespace Acme.Automation.WebRunner
             }
 
             app.UseHangfireDashboard();
+            this.StartAutomation();
 
             app.Run(async context => { await context.Response.WriteAsync("Hello World!"); });
         }
@@ -59,6 +73,29 @@ namespace Acme.Automation.WebRunner
 
             // Add the processing server as IHostedService
             services.AddHangfireServer();
+        }
+
+        private void StartAutomation()
+        {
+            var configurationPath = Path.Combine(this.Environment.ContentRootPath, "configuration.json");
+
+            if (!File.Exists(configurationPath))
+            {
+                Log.Error($"Cannot find the configuration file {configurationPath} to register jobs.");
+                return;
+            }
+
+            var configuration = AutomationConfiguration.Read(configurationPath);
+
+            foreach (var job in configuration.Jobs.Where(job => job.RunAtStartup))
+            {
+                BackgroundJob.Enqueue<AutomationRunner>(automation => automation.Execute(configuration, job));
+            }
+
+            foreach (var job in configuration.Jobs.Where(job => !string.IsNullOrWhiteSpace(job.CronSchedule)))
+            {
+                RecurringJob.AddOrUpdate<AutomationRunner>(job.Id, automation => automation.Execute(configuration, job), job.CronSchedule, TimeZoneInfo.Utc);
+            }
         }
     }
 }
